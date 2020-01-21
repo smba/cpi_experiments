cb433c53616bcbd9c1b2f2ca897a20bc5dea2979
Stefan <stefan.muehlbauer@uni-weimar.de>
2020-01-21 17:31:44 +0100

    ¯\_(ツ)_/¯

--- local-slurm.py
+++ local-slurm.py
@@ -4,27 +4,20 @@ import sys
 import time 
 import statistics as stat
 
-
-print("es flutet")
-
 PROJECT = sys.argv[1]
 REPETITIONS = int(sys.argv[2])
 TIMEOUT = int(sys.argv[3])
 ARRAY_ID = int(sys.argv[4])
 
-
 # gehe zu tmp
 os.chdir("/home/stefan/")
 
-#os.system("rm -rf *")
+# mach ma sauber
+os.system("rm -rf *")
 
 if not "case_studies" in os.listdir("."):
 	print("hier flutet es auch")
 	os.system("git clone https://github.com/smba/cpi_experiments.git case_studies")
-#print(os.listdir("."))
-#print(os.getcwd())
-
-print("jojojojojo")
 
 # Obtain binaries
 os.chdir("/home/stefan/case_studies")
@@ -33,14 +26,12 @@ os.system("git pull")
 # get access to  binaries
 os.chdir("{}".format(PROJECT))
 
-print("flut flut mothafluters")
-
 pwd = os.getcwd()
 BINARIES = os.listdir(".")
 BINARIES_ABSOLUTE = [pwd + "/" + binary for binary in BINARIES]
 os.chdir("..")
 
-# obtain config
+# obtain configs
 configs = open("{}_configs.txt".format(PROJECT), "r").readlines()
 config = configs[ARRAY_ID].replace("\n", "")
 config_tail = config.split(" ")
@@ -50,29 +41,25 @@ os.system("rm -rf {}".format(fname))
 
 for j, binary in enumerate(BINARIES):
 	rev_index = int(binary.split("_")[0]) # revision id
-	print("hier")	
 	durations = []
 	for i in range(REPETITIONS): 
-		#time.sleep(2)
+		time.sleep(2)
 		start = time.time()
-		#print("timeout {} ./{}".format(TIMEOUT, BINARIES_ABSOLUTE[j]) + " ".join(config_tail))
 		try:
 			subprocess.run([
 				"timeout", 
 				"%d" % TIMEOUT, 
 				"{}".format(BINARIES_ABSOLUTE[j])] + config_tail, check=True)
 		except:
-			#time.sleep(3)
+			time.sleep(2)
 			pass
 
 		end=time.time()
 		duration = end - start
 		durations.append(duration)
-	   
-	print(durations)
+
 	median = stat.median(durations)
-	
-	#fname = "perf_{}_{}.csv".format(PROJECT, ARRAY_ID)
+
 	f = open(fname, "a+")
 	f.write("{},{}\n".format(rev_index, median))
 	f.close()
